5fe73e58c70f4cc1556cca207b311c945a8820d3
krpando <karpando@gmail.com>
2018-11-23 00:27:06 -0500

    style complete, htlm complete, javascript is running correctly, cipher si amazing!

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,4 @@
-window.cipher = {
+//windows.cipher{
 //Función Cifrar
   const encode = (offset, string) => {
     let textOut=new Array();
@@ -9,7 +9,7 @@ window.cipher = {
         } else
          textOut.push(String.fromCharCode(`${(coded-65+offset)%26+65}`));
      } return textOut.join("");
-  };
+    };
 //Función Descifrar
   const decode = (offset, string) => {
     let textOut=new Array();
@@ -18,7 +18,7 @@ window.cipher = {
         if(string.charCodeAt(i)===32){
          textOut.push(" ");
         } else
-         textOut.push(String.fromCharCode(`${(coded-65-offset)%26+65}`));
+         textOut.push(String.fromCharCode(`${(coded-90-offset)%26+90}`));
      } return textOut.join("");
   };
-};
+//};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
+//Visibilidad de secciones
+document.getElementById('section1').style.display='block';
+document.getElementById('section2').style.display='none';
+document.getElementById('section3').style.display='none';
 //Botones cifrar y descifrar
-let textIn=document.getElementById('msg');
-let keySet=document.getElementById('codi');
-let buttonMsgOn=document.getElementById('cif');
-let buttonMsgOff=document.getElementById('des');
+let msgIn=document.getElementById('msg');
+let keyNumber=document.getElementById('key');
+let buttonMsgIn=document.getElementById('enco');
+let buttonMsgOut=document.getElementById('deco');
 let msgOut=document.getElementById('result');
-
-buttonMsgOn.addEventListener('click', ()=>{
-  msgOut.innerHTML=encode(parseInt(keySet.value),textIn.value);
+//Correr función cifrar
+buttonMsgIn.addEventListener('click', ()=>{
+  msgOut.innerHTML=encode(parseInt(keyNumber.value),msgIn.value);
+  document.getElementById('personalresult').innerHTML='Listo! ahora es secreto ( ͡° ͜ʖ ͡°)';
+  document.getElementById('section1').style.display='none';
+  document.getElementById('section2').style.display='block';
 });
-
-buttonMsgOff.addEventListener('click', ()=>{
-  msgOut.innerHTML=decode(parseInt(keySet.value),textIn.value);
+//Correr función descifrar
+buttonMsgOut.addEventListener('click', ()=>{
+  msgOut.innerHTML=decode(parseInt(keyNumber.value),msgIn.value);
+  document.getElementById('personalresult').innerHTML='Uy! que interesante ( ͡° ͜ʖ ͡°)';
+  document.getElementById('section1').style.display='none';
+  document.getElementById('section2').style.display='block';
 });
-//Botones "Seguir jugando" y "Quiero estudiar"
-const jugando=document.getElementById('playing');
-const estudiando=document.getElementById('studying');
-jugando.addEventListener('click', ()=>{
-  
+//Botón retornar al Menú
+document.getElementById('start').addEventListener('click', ()=>{
+  document.getElementById('section2').style.display='none';
+  document.getElementById('section1').style.display='block';
 });
+//Botón ocultarse
+document.getElementById('hide').addEventListener('click', ()=>{
+   document.title = 'Me gusta estudiar :v';
+   document.body.style.backgroundImage = 'none';
+   document.body.style.backgroundColor = '#000000';
+   document.getElementById('section2').style.display='none';
+   document.getElementById('section3').style.display='block';
+});
+//"Continuar" -> "Volver al encryptioner"
+document.getElementById('continue').addEventListener('click', ()=>{
+  document.getElementById('section3').style.display='none';
+  document.getElementById('section2').style.display='block';
+});
+//"Quiero estudiar"
+document.getElementById('study').addEventListener('click', ()=>{
+  document.getElementById('section3').style.display='none';
+});
+//https://web.whatsapp.com/
