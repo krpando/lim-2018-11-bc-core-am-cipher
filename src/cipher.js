38cb4e6c28938dde76b04e9dfafbf6d4b0c61c2e
krpando <karpando@gmail.com>
2018-11-27 08:46:29 -0500

    Responsive (finally) and lowercase, comma and dot cipher done

--- src/cipher.js
+++ src/cipher.js
@@ -4,10 +4,16 @@ window.cipher = {
     let textOut=new Array();
      for(let i=0;i<string.length;i+=1){
         let coded=parseInt(string.charCodeAt(i));
-        if(string.charCodeAt(i)===32){
-         textOut.push(' ');
-        } else
-         textOut.push(String.fromCharCode(`${(coded-65+offset)%26+65}`));
+          if(string.charCodeAt(i)===32){
+            textOut.push(' ');
+          } else if(string.charCodeAt(i)===46){
+            textOut.push('.');
+          } else if(string.charCodeAt(i)===44){
+            textOut.push(',');
+          } else if(string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+            textOut.push(String.fromCharCode(`${(coded-97+offset)%26+97}`));
+          } else
+            textOut.push(String.fromCharCode(`${(coded-65+offset)%26+65}`));
      } return textOut.join('');
    },
 //Función Descifrar
@@ -15,10 +21,16 @@ window.cipher = {
     let textOut=new Array();
      for(let i=0;i<string.length;i+=1){
         let coded=parseInt(string.charCodeAt(i));
-        if(string.charCodeAt(i)===32){
-         textOut.push(' ');
-        } else
-         textOut.push(String.fromCharCode(`${(coded-90-offset)%26+90}`));
+          if(string.charCodeAt(i)===32){
+            textOut.push(' ');
+          } else if(string.charCodeAt(i)===46){
+            textOut.push('.');
+          } else if(string.charCodeAt(i)===44){
+            textOut.push(',');
+          } else if(string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+            textOut.push(String.fromCharCode(`${(coded-122-offset)%26+122}`));
+          } else
+            textOut.push(String.fromCharCode(`${(coded-90-offset)%26+90}`));
      } return textOut.join('');
   }
 };
